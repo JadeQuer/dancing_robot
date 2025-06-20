c4c36c502dd96c6176a2628a45a32ab592e425e0
robotjiang1 <jiangqinru@mail.nwpu.edu.cn>
2025-06-20 18:09:43 +0800

    韵律识别

--- rhythm_recog.py
+++ rhythm_recog.py
@@ -53,9 +53,9 @@ def play_audio(audio_file):
 
 def rhythm_recog():
     play_audio('resources/开始韵律识别.wav')
-    record_music('./temp_record/song_temp.MP3', time=15)
+    record_music('./temp_record/song_temp.MP3', time=13)
     play_audio('resources/结束录音.wav')
-    all_data = np.load('beatDatabase.npy', allow_pickle=True)
+    all_data = np.load('rhythm_train/beatDatabase.npy', allow_pickle=True)
     beat_database = all_data.item()
 
     testAudio = "./temp_record/song_temp.MP3"
